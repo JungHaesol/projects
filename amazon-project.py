b70a558bea05d5e2ec87e4db8b18dae5fbc840ee
JungHaesol <88611054+JungHaesol@users.noreply.github.com>
2021-08-12 17:17:38 +0900

    Update and rename spiderproject3.py to amazon-project.py

--- amazon-project.py
+++ amazon-project.py
@@ -1,5 +1,10 @@
 # 정해솔 
 
+# 해외의 온라인 매장인 아마존을 스크래핑해볼 것이다. 아마존은 위키백과, 모의 스크래핑을 위한 온라인 서점 사이트와는 다르게 오로지 requests와 
+# BeautifulSoup로만 스크래핑이 안된다. 따라서 Selenium과 구글 크롬의 웹 드라이버를 이용하여 정보를 가져오고 특정한 창을 열어보는 연습을 
+# 할 것이다. 
+
+
 from selenium import webdriver 
 import chromedriver_binary 
 import csv
